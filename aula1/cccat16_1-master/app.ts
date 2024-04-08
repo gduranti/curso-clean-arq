7f415807d0ac54f4884acedd52958910ac6fdf3c
Gabriel Duranti <gabriel-duranti@procergs.rs.gov.br>
2024-04-08 15:40:25 -0300

    aula 1

---
+++ aula1/cccat16_1-master/app.ts
@@ -0,0 +1,13 @@
+import { helloWorld } from "./src/hello-world"
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send(helloWorld())
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
